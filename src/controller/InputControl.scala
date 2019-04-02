38b3785a9dd9fb34dc9dac7b8fe8a6e862a973f0
fieryrob <rbamm@buffalo.edu>
2019-04-02 15:42:25 -0400

    Add captions and image source to model file

--- src/controller/InputControl.scala
+++ src/controller/InputControl.scala
@@ -1,5 +1,20 @@
 package controller
 
+import javafx.event.{ActionEvent, EventHandler}
+import model.gameRules
+
 class InputControl {
 
+}
+
+class cardSubmitted(text: String) extends EventHandler[ActionEvent]{
+  override def handle(event: ActionEvent): Unit = {
+
+  }
+}
+
+class cardSelected(text: String) extends EventHandler[ActionEvent]{
+  override def handle(event: ActionEvent): Unit = {
+
+  }
 }
